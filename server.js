555aecff70563084337ead09f4fd1d832e534db9
Hower He <howerhe@outlook.com>
2021-08-05 15:24:10 -0400

    Add cross-origin resource sharing

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // TODO: add authentications.
